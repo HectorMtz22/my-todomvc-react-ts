82c261dd8c163541c8eb0164bbdc0dc7e7fb6132
HectorMtz22 <hec.gre81@gmail.com>
2022-04-06 23:32:33 -0500

    Separate component and center the todoapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import ListTodos from 'components/ListTodos'
 
 function App () {
   return (
@@ -10,22 +11,8 @@ function App () {
           placeholder='What needs to be done?'
         />
       </header>
+      <ListTodos />
 
-      <section className='main'>
-        <input id='toggle-all' type='checkbox' className='toggle-all' />
-        <label htmlFor='toggle-all' />
-
-        <ul className='todo-list'>
-          <li className='todo-item'>
-            <div className='view'>
-              <input type='checkbox' className='toggle' />
-              <label onDoubleClick={() => window.alert('Mock')}>HI</label>
-              <button className='destroy' onClick={() => window.alert('otro click')} />
-            </div>
-            <input type='text' className='edit' />
-          </li>
-        </ul>
-      </section>
       <footer className='footer'>
         <span className='todo-count'>
           {/* ! TODO  */}
